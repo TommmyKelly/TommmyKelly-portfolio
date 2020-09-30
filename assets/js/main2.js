5ac6d550d344ac5186999327fc409e89c9407f41
TommmyKelly <tommy_kelly@icloud.com>
2020-09-30 11:43:45 +0100

    adding animations

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,9 +1,21 @@
 const contactInfo = document.getElementById('contact-info')
 const sideBar = document.getElementById('sidebar')
 const inner = document.getElementById('inner')
+const mainImage = document.getElementById('main-image')
+const nameTitle = document.getElementById('name')
 
+
+window.addEventListener('resize',function(){
+    location.reload()
+})
+
+// $(window).resize(function() {
+//     location.reload();
+// });
 
 inner.addEventListener('click',(e)=>{
+    if (breakpoints.active('>large'))
+							return;
   if(e.target.parentElement.id == "contact-info"){
     sideBar.classList.remove('inactive')
 }else{
@@ -12,6 +24,8 @@ inner.addEventListener('click',(e)=>{
 })
 
 contactInfo.addEventListener('click',(e)=>{
+    if (breakpoints.active('>large'))
+							return;
     if(e.target.parentElement.id == "contact-info"){
         sideBar.classList.remove('inactive')
         console.log('true')
@@ -21,3 +35,71 @@ contactInfo.addEventListener('click',(e)=>{
     console.log('false')
     
 })
+
+
+
+const TypeWriter = function(txtElement, words, wait = 2000){
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex ='';
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isdeleting = false;
+}
+
+TypeWriter.prototype.type = function(){
+
+   
+
+    const current = this.wordIndex % this.words.length;
+
+    
+
+    const fulltxt = this.words[current];
+    
+  
+
+if(this.isdeleting){
+    this.txt = fulltxt.substring(0,this.txt.length -1);
+    
+}else{
+    this.txt = fulltxt.substring(0,this.txt.length +1);
+    
+}
+
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
+
+    let typeSpeed = 300;
+
+    if(this.isdeleting){
+        typeSpeed /=2
+    }
+
+    if(!this.isdeleting && this.txt === fulltxt){
+        typeSpeed = this.wait;
+        this.isdeleting = true
+    }else if(this.isdeleting && this.txt === ''){
+        this.addEventListener = false;
+        this.isdeleting=false
+        this.wordIndex ++
+
+        typeSpeed = 500
+    }
+
+     setTimeout(() => this.type(),typeSpeed)
+}
+
+
+
+function init(){
+    const txtElement = document.querySelector('.java-script-text');
+    const words = JSON.parse(txtElement.getAttribute('data-words'))
+    console.log(words)
+    const wait = txtElement.getAttribute('data-wait')
+
+    new TypeWriter(txtElement, words, wait)
+}
+
+mainImage.addEventListener('animationend',()=> mainImage.classList.add('jello-horizontal'))
+nameTitle.addEventListener('animationend',init)
